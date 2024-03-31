d84890ee69dc3fe6b0bbd6b49b9ad040649b1edf
Roy Wood <roy.wood@gmail.com>
2024-03-31 10:50:39 -0400

    Hey, that worked pretty easily

--- main_gtk4_columnview.py
+++ main_gtk4_columnview.py
@@ -11,24 +11,43 @@ class MyListModelDataItem(GObject.Object):
     def __init__(self, user_id, user_name_first, user_name_last):
         super().__init__()
 
-        self._user_id = user_id
-        self._user_name_first = user_name_first
-        self._user_name_last = user_name_last
+        self.user_id = user_id
+        self.user_name_first = user_name_first
+        self.user_name_last = user_name_last
 
-    @GObject.Property(type=str)
-    def user_id(self):
-        return self._user_id
-
-    @GObject.Property(type=str)
-    def user_name_first(self):
-        return self._user_name_first
-
-    @GObject.Property(type=str)
-    def user_name_last(self):
-        return self._user_name_last
+    # @GObject.Property(type=str)
+    # def user_id(self):
+    #     return self._user_id
+    #
+    # @GObject.Property(type=str)
+    # def user_name_first(self):
+    #     return self._user_name_first
+    #
+    # @GObject.Property(type=str)
+    # def user_name_last(self):
+    #     return self._user_name_last
 
     def __repr__(self):
-        return f"MyListModelDataItem(id={self._user_id}, user_name={self._user_name_first} {self._user_name_last})"
+        return f"MyListModelDataItem(id={self.user_id}, user_name={self.user_name_first} {self.user_name_last})"
+
+
+class MyItemFactory(Gtk.SignalListItemFactory):
+    def __init__(self, field_name):
+        super().__init__()
+        self._field_name = field_name
+        self.connect("setup", self.on_setup)
+        self.connect("bind", self.on_bind)
+
+
+    def on_setup(self, _signal_factory, list_item):
+        label = Gtk.Label.new()
+        label.set_halign(Gtk.Align.START)
+        list_item.set_child(label)
+
+    def on_bind(self, _signal_factory, list_item):
+        my_list_mode_data_item = list_item.props.item
+        label = list_item.get_child()  # label
+        label.props.label = str(getattr(my_list_mode_data_item, self._field_name))
 
 
 class MainWindow(Gtk.ApplicationWindow):
@@ -41,18 +60,19 @@ class MainWindow(Gtk.ApplicationWindow):
         self.list_store_model.append(MyListModelDataItem(2, 'Bart', 'Simpson'))
         self.list_store_model.append(MyListModelDataItem(3, 'James', 'Bond'))
 
-        self.scrolled_window = Gtk.ScrolledWindow.new()
-        self.set_child(self.scrolled_window)
+        # self.listview = Gtk.ListView.new(self.single_selection_list_store, self.signal_factory)
 
-        # # ColumnView with custom columns
-        # self.columnview = Gtk.ColumnView()
-        # self.columnview.set_show_column_separators(True)
-        # data = [f'Data Row: {row}' for row in range(50)]
-        # for i in range(4):
-        #     column = Gtk.ColumnViewColumn()
-        #     column.set_title(f"Column {i}")
-        #     self.columnview.append_column(column)
-        # self.scrolled_window.set_child(self.columnview)
+        # ColumnView with custom columns
+        self.column_view = Gtk.ColumnView(model=Gtk.SingleSelection(model=self.list_store_model), hexpand=True, vexpand=True)
+        # self.column_view.set_show_column_separators(True)
+        self.column_view.set_show_row_separators(True)
+        self.column_view.append_column(Gtk.ColumnViewColumn(title='ID', factory=MyItemFactory('user_id'), expand=True))
+        self.column_view.append_column(Gtk.ColumnViewColumn(title='FIRST', factory=MyItemFactory('user_name_first'), expand=True))
+        self.column_view.append_column(Gtk.ColumnViewColumn(title='ID', factory=MyItemFactory('user_name_last'), expand=True))
+
+        self.scrolled_window = Gtk.ScrolledWindow.new()
+        self.scrolled_window.set_child(self.column_view)
+        self.set_child(self.scrolled_window)
 
 
 class MyApp(Gtk.Application):
